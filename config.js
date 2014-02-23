c8595793c0b2c7c28e492a1a9379129f9ecce9d6
virgoboy2004 <wenjiezhang2013@gmail.com>
2014-02-23 12:44:27 -0800

    Refresh

--- config.js
+++ config.js
@@ -36,7 +36,7 @@ config = {
             // Host to be passed to node's `net.Server#listen()`
             host: '0.0.0.0',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: process.env.PORT
+            port: '8080'
         }
     },
 
