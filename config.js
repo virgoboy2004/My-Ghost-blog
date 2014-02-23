5c7c546c53c0c05cd5d6a055e6c9699daaf28ab4
virgoboy2004 <wenjiezhang2013@gmail.com>
2014-02-23 09:20:57 -0800

    Try

--- config.js
+++ config.js
@@ -38,9 +38,9 @@ config = {
         },
         server: {
             // Host to be passed to node's `net.Server#listen()`
-            host: '127.0.0.1',
+            host: '0.0.0.0',
             // Port to be passed to node's `net.Server#listen()`, for iisnode set this to `process.env.PORT`
-            port: 1111
+            port: process.env.PORT
         }
     },
 
